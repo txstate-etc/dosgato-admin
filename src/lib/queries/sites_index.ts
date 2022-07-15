7d58cb9f66a22def3a86e4ea701540b4c9423310
Anne Faber <anne.faber@txstate.edu>
2022-07-15 10:09:10 -0500

    start site management interface

--- src/lib/queries/sites_index.ts
+++ src/lib/queries/sites_index.ts
@@ -1,11 +1,35 @@
 export const siteDetails = `
   id
   name
+  url {
+    host
+    path
+    prefix
+  }
+  organization {
+    name
+  }
+  owner {
+    id
+    name
+  }
 `
 
 export interface SiteListSite {
   id: string
   name: string
+  url: {
+    host: string
+    path: string
+    prefix: string
+  }
+  organization: {
+    name: string
+  }
+  owner: {
+    id: string
+    name: string
+  }
 }
 
 export const GET_SITE_LIST = `
--- src/routes/sites/index.svelte
+++ src/routes/sites/index.svelte
@@ -1,1 +1,29 @@
-site management interface
+<script lang="ts" context="module">
+  import applicationOutline from '@iconify-icons/mdi/application-outline'
+  type TypedSiteItem = TypedTreeItem<SiteListSite>
+
+  async function fetchChildren (site?: TypedSiteItem) {
+    if (site) return []
+    const siteList = await api.getSiteList()
+    return siteList
+  }
+  function renderOwner (site: TypedSiteItem) {
+    return `${site.owner.name} (${site.owner.id})`
+  }
+  const store: TreeStore<SiteListSite> = new TreeStore(fetchChildren)
+</script>
+
+<script lang="ts">
+import { api, ActionPanel, Tree, TreeStore, type TypedTreeItem, type SiteListSite } from '$lib'
+</script>
+
+<ActionPanel actions={[]}>
+  <Tree {store} headers={[
+     { id: 'name', label: 'Site Name', get: 'name', defaultWidth: '20%', icon: applicationOutline },
+     { id: 'url', label: 'URL', get: 'url.prefix', defaultWidth: '20%' },
+     { id: 'organization', label: 'Organization', get: 'organization.name', defaultWidth: '20%' },
+     { id: 'owner', label: 'Owner', render: renderOwner, defaultWidth: '20%' }
+  ]}>
+
+  </Tree>
+</ActionPanel>
