d7dbdc60cf11d6157229e36d6ecc4dd1b74aa76f
Nick Wing <nwing@txstate.edu>
2022-08-18 15:14:27 -0500

    work around redirect problem on /admin

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,7 +1,10 @@
 import { redirect } from '@sveltejs/kit'
-import type { PageLoad } from './$types'
+import { browser } from '$app/env'
+import { goto } from '$app/navigation'
 import { base } from '$app/paths'
+import type { PageLoad } from './$types'
 
 export const load: PageLoad = async () => {
-  throw redirect(301, base + '/pages')
+  if (browser) await goto(base + '/pages') // temporary while they fix throw redirect https://github.com/sveltejs/kit/issues/5952
+  else throw redirect(301, base + '/pages')
 }
