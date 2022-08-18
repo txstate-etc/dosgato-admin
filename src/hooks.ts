29c902f9abcf2fd065bea7f00cf3d9fb23dbc548
Nick Wing <nwing@txstate.edu>
2022-08-18 15:14:44 -0500

    simplify type imports on hooks.ts

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,11 +1,11 @@
-import type { MaybePromise, RequestEvent, ResolveOptions } from '@sveltejs/kit/types/internal'
+import type { RequestEvent, ResolveOptions } from '@sveltejs/kit'
 
 export async function handle ({ event, resolve }: {
   event: RequestEvent
   resolve: (
     event: RequestEvent,
     opts?: ResolveOptions
-  ) => MaybePromise<Response>
+  ) => Promise<Response>
 }) {
   return await resolve(event, {
     ssr: false
