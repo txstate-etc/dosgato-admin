0adc9a8057a75dcee062e245956e7a0d4c450f36
Anne Faber <anne.faber@txstate.edu>
2022-09-02 15:52:01 -0500

    Fix subnav issue in Access area.

--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -1,17 +1,1 @@
-<script lang="ts" context="module">
-  import { base } from '$app/paths'
-  import { subnav } from '$lib/stores/global'
-  import accountIcon from '@iconify-icons/mdi/account'
-  import accountWrenchIcon from '@iconify-icons/mdi/account-wrench'
-  import accountGroup from '@iconify-icons/mdi/account-group'
-  import keyChain from '@iconify-icons/mdi/key-chain'
-
-  subnav.set([
-    { label: 'Users', href: base + '/auth/users', icon: accountIcon },
-    { label: 'System', href: base + '/auth/system', icon: accountWrenchIcon },
-    { label: 'Groups', href: base + '/auth/groups', icon: accountGroup },
-    { label: 'Roles', href: base + '/auth/roles', icon: keyChain }
-  ])
-</script>
-
 <slot />
---
+++ src/routes/auth/+layout.ts
@@ -0,0 +1,15 @@
+import { base } from '$app/paths'
+import { subnav } from '$lib/stores/global'
+import accountIcon from '@iconify-icons/mdi/account'
+import accountWrenchIcon from '@iconify-icons/mdi/account-wrench'
+import accountGroup from '@iconify-icons/mdi/account-group'
+import keyChain from '@iconify-icons/mdi/key-chain'
+
+export const load = async () => {
+  subnav.set([
+    { label: 'Users', href: base + '/auth/users', icon: accountIcon },
+    { label: 'System', href: base + '/auth/system', icon: accountWrenchIcon },
+    { label: 'Groups', href: base + '/auth/groups', icon: accountGroup },
+    { label: 'Roles', href: base + '/auth/roles', icon: keyChain }
+  ])
+}
