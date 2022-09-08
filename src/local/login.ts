d2e57634dbe190383f2ac1db7418a904a29abb87
Nick Wing <nwing@txstate.edu>
2022-09-08 08:44:50 -0500

    update to use unified-auth's requestedUrl instead of returnUrl as returnUrl is meant to always be the same and match unified-auth's database entry for the client_id

--- src/local/login.ts
+++ src/local/login.ts
@@ -2,8 +2,14 @@ import { type LoadEvent, redirect } from '@sveltejs/kit'
 
 export function handleUnauthorized (environmentConfig: any, response: any) {
   const authRedirect = new URL(environmentConfig.authRedirect)
-  authRedirect.searchParams.set('returnUrl', location.href)
+  authRedirect.searchParams.set('returnUrl', `${location.origin}/admin`)
+  authRedirect.searchParams.set('requestedUrl', location.href)
   throw redirect(301, authRedirect.toString())
+}
+
+export function getRedirect ({ url }: LoadEvent) {
+  const { searchParams } = url
+  return searchParams.get('requestedUrl')
 }
 
 export function getToken ({ url }: LoadEvent) {
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,6 @@
-import { error, type Load } from '@sveltejs/kit'
+import { error, redirect, type Load } from '@sveltejs/kit'
 import { api, globalStore } from '$lib'
-import { getToken } from '../local'
+import { getRedirect, getToken } from '../local'
 
 export const load: Load = async (input) => {
   api.fetch = input.fetch
@@ -10,6 +10,8 @@ export const load: Load = async (input) => {
     const { me, access } = await api.getSelf()
     if (!me) throw error(403, 'You are not authorized to use this system.')
     globalStore.update(v => ({ ...v, me, access }))
+    const redirectUrl = getRedirect(input)
+    if (redirectUrl) throw redirect(301, redirectUrl)
   } catch (e: any) {
     if (e.status === 301) throw e
     errObj = e
