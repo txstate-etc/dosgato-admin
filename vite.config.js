6fde9920c137dce87998261331a48c862bb0c38c
Nick Wing <nwing@txstate.edu>
2022-09-09 18:47:24 -0500

    go ahead and add sourcemap markings to regular js files, looks necessary

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
-  build: { sourcemap: 'hidden' },
+  build: { sourcemap: true },
   css: { devSourcemap: true }
 };
 
