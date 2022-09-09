1976e2b0efb97496f979f92e0426579647156788
Nick Wing <nwing@txstate.edu>
2022-09-09 18:33:13 -0500

    generate sourcemaps during the svelte build

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,8 @@ import preprocess from 'svelte-preprocess'
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess({ sourceMap: true }),
+	compilerOptions: { enableSourcemap: true },
 	kit: {
 		adapter: adapter({
 			fallback: 'index.html',
