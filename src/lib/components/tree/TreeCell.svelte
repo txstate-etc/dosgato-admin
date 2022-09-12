0f048b0e1ec46ac71607cbc2b1265180122be0b4
Nick Wing <nwing@txstate.edu>
2022-09-12 11:00:23 -0500

    make trees a little more attractive with larger icons

--- src/lib/components/tree/TreeCell.svelte
+++ src/lib/components/tree/TreeCell.svelte
@@ -11,7 +11,7 @@
 </script>
 
 {#if header.icon}
-  <span class="icon"><Icon {icon} inline /></span>
+  <span class="icon"><Icon {icon} inline width="1.5em" /></span>
 {/if}
 {#if header.component}
   <svelte:component this={header.component} {item} {header} />
