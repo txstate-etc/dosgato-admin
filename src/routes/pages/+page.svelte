f098cb4bdae0ef3030de2460bff6119d820ffd3a
kevinpena <kep85@txstate.edu>
2023-04-20 10:51:14 -0500

    adding label to page status icon

--- src/routes/pages/+page.svelte
+++ src/routes/pages/+page.svelte
@@ -251,7 +251,7 @@
       { label: 'Path', id: 'name', grow: 4, icon: item => ({ icon: item.deleteState === DeleteState.MARKEDFORDELETE ? deleteEmpty : item.parent ? browserIcon : siteIcon[item.type] }), get: 'name' },
       { label: 'Title', id: 'title', grow: 3, get: 'title' },
       { label: 'Template', id: 'template', fixed: '8.5em', get: 'template.name' },
-      { label: 'Status', id: 'status', fixed: '4em', icon: item => ({ icon: item.deleteState === DeleteState.NOTDELETED ? statusIcon[item.status] : deleteOutline }), class: item => item.deleteState === DeleteState.NOTDELETED ? item.status : 'deleted' },
+      { label: 'Status', id: 'status', fixed: '4em', icon: item => ({ icon: item.deleteState === DeleteState.NOTDELETED ? statusIcon[item.status] : deleteOutline, label: item.deleteState === DeleteState.NOTDELETED ? item.status : 'deleted' }), class: item => item.deleteState === DeleteState.NOTDELETED ? item.status : 'deleted' },
       { label: 'Modified', id: 'modified', fixed: '10em', render: item => `<span class="full">${dateStamp(item.modifiedAt)}</span><span class="short">${dateStampShort(item.modifiedAt)}</span>` },
       { label: 'By', id: 'modifiedBy', fixed: '4.3em', get: 'modifiedBy.id' }
     ]} searchable={['name', 'title']}
