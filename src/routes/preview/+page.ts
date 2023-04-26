0ce73875e6389f897ee853d581691bf19e82a1a6
Nick Wing <nwing@txstate.edu>
2023-04-26 11:10:56 -0500

    yet another attempt

--- src/routes/preview/+page.ts
+++ src/routes/preview/+page.ts
@@ -5,7 +5,7 @@ import { isNotBlank } from 'txstate-utils'
 export const load: Load = async input => {
   await input.parent()
   if (api.token && isNotBlank(input.url.searchParams.get('url'))) {
-    const requestedUrl = new URL(input.url.searchParams.get('url')!, window.location.protocol + '://' + window.location.host).toString()
+    const requestedUrl = new URL(input.url.searchParams.get('url')!, window.location.protocol + '//' + window.location.host).toString()
     const rBase = new URL(environmentConfig.renderBase, location.href).toString()
     const preview = new URL('.preview', rBase).toString()
     const compare = new URL('.compare', rBase).toString()
