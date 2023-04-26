9982cb174889f936e06e21cdddf35d8629f64c51
Nick Wing <nwing@txstate.edu>
2023-04-26 09:02:13 -0500

    fix error when trying to preview in new page

--- src/routes/preview/+page.ts
+++ src/routes/preview/+page.ts
@@ -6,7 +6,12 @@ export const load: Load = async input => {
   await input.parent()
   if (api.token && isNotBlank(input.url.searchParams.get('url'))) {
     const requestedUrl = new URL(input.url.searchParams.get('url')!, window.location.href).toString()
-    if (!requestedUrl.startsWith(environmentConfig.renderBase + '/.preview/') && !requestedUrl.startsWith(environmentConfig.renderBase + '/.compare/')) throw error(400, 'This path only helps with preview and compare links.')
+    if (
+      !requestedUrl.startsWith('/.preview/') &&
+      !requestedUrl.startsWith('/.compare/') &&
+      !requestedUrl.startsWith(environmentConfig.renderBase + '/.preview/') &&
+      !requestedUrl.startsWith(environmentConfig.renderBase + '/.compare/')
+    ) throw error(400, 'This path only helps with preview and compare links.')
     const parsedUrl = new URL(requestedUrl, input.url)
     parsedUrl.searchParams.append('token', api.token)
     throw redirect(302, parsedUrl.toString())
