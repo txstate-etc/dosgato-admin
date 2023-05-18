b1aa284cf53f38f653bc268f02c8c2b96d5533b8
Nick Wing <nwing@txstate.edu>
2023-05-18 14:40:03 -0500

    need to enhance template information when visiting the sites UI in case we make a new one

--- src/routes/sites/+layout.ts
+++ src/routes/sites/+layout.ts
@@ -1,8 +1,9 @@
 import listDashes from '@iconify-icons/ph/list-dashes'
 import type { Load } from '@sveltejs/kit'
 import { base } from '$app/paths'
-import { subnavStore } from '$lib'
+import { subnavStore, templateRegistry } from '$lib'
 
 export const load: Load = async () => {
   subnavStore.init('sites', [{ label: 'Sites', href: base + '/sites', icon: listDashes }])
+  await templateRegistry.enhanceInfo()
 }
