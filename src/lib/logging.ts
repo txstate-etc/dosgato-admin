6f16ab343404c88d0c64ac497a7a9885777f0d6a
Drew Pilling (aap64) <Drew.Pilling@TxState.edu>
2023-05-26 15:56:41 -0500

    Added environmentConfig to uiLogger when it's not just the console.log function.

--- src/lib/logging.ts
+++ src/lib/logging.ts
@@ -1,30 +1,41 @@
 import type { ITreeStore } from '@dosgato/dialog'
-import { uiConfig } from '../local'
-
+// import type { BaseEvent } from '@dosgato/templating'
+import { environmentConfig } from './stores'
 
 export interface BaseEvent {
-  /** The larger UI area the user is interacting with. Admin or ComponetDialog for instance */
-  // May need to do a translation between component id/name to colloquial names.
-  eventType: string
+  /** The larger UI area the user is interacting with that the event is emitted from.
+   * @example 'ActionPanel', 'PageEditor', 'ComponentDialog' */
+  eventType: string // How about renaming to `emitterContext`?
 
-  // The specific action the user took. AddPage or SaveComponent for instance
+  /** The specific action the user took. Typically the label for the element that emits
+   * the event.
+   * @example 'Add Page', 'Edit Page', 'Preview', 'Cancel Preview' */
   action: string
 
-  // Additional data points specific to an event. Keep it simple please
-  // Not a good place for catch all details, we want to keep the overall structure here
-  // efficiently simple so we want to avoid catch all logging in general. This is for
-  // additional details we absolutely must have.
-  // Whatever we put in here becomes a column in a table in elasticsearch.
+  /** Additional data points specific to a particular event type's context. These should
+   * be significant enough to understanding the event to merrit adding additional columns
+   * in tools like elastic-search.
+   * @warning This is NOT a catch-all property.
+   * @example { hiddenLabel: action.hiddenLabel } // The aria label for an action element. */
   additionalProperties?: Record<string, string | undefined>
 }
 
+/** Events triggered by user interactions with interface elements in DosGato. This interface
+ * is intended to provide a common model for succinctly expressing the contextually important
+ * properties of these events to loggers that can be pointed to analytics and metrics services. */
 export interface UserEvent extends BaseEvent {
-  // The context in which the action occured. Often a page or compoent path
-  // Example: if they're adding a page, where are they adding the page.
-  // Set at the big layout level.
+  /** The page, screen, or dialog the user is looking at in which the associated event emitter is
+   * in context to.
+   * @example '/pages', '/pages/[id]', '/pages/[id]/dialog' */
   screen: string
 
-  // Each screen needs to set their target for what actions are targetd to.... Page or Component we're operating on.
+  /** The target the emitted event is to trigger actions on.
+   * Each page/screen, or dialog, needs to set their target for what events in it are targeted
+   * to act on in in its context.
+   *
+   * For example: The page in the page tree of the Pages screen that ActionPanel actions,
+   * such as edit or preview, will act on.
+   * @example '/site3-sandbox/about' */
   target: string
 }
 
@@ -34,11 +45,15 @@ export interface UserEvent extends BaseEvent {
 class InteractionLogger {
   screen: string | undefined
   target: string | undefined
-  logger: (info: any) => void
+  logger: (...args: any[]) => void
 
   log (info: BaseEvent) {
+    // Don't log anything if we've been asked to log without a screen defined to give context.
     if (!this.screen) return
-    this.logger({ ...info, screen: this.screen, target: this.target ?? '' })
+    const logInfo = { ...info, screen: this.screen, target: this.target ?? '' }
+    if (this.logger === console.log) {
+      this.logger(logInfo)
+    } else this.logger(logInfo, environmentConfig)
   }
 
   targetFromTreeStore (state: ITreeStore<any>, prop: string) {
