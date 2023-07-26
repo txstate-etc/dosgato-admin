d993a4fabf159380f13861c35cfa9b7f0a97bd98
Yuanhua Qu <yuanhuaqu@yahoo.com>
2023-07-26 10:58:16 -0500

    Diable tests parallelism before finding multiworker auth solution

--- playwright.config.js
+++ playwright.config.js
@@ -20,7 +20,8 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  // workers: process.env.CI ? 1 : undefined,
+  workers:  1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html', { open: 'on-failure', outputFolder: './tests/report' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
