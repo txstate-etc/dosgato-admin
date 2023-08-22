dccadf19b6fa24604bd714679c11c78a6b4f6ecb
Yuanhua Qu <yuanhuaqu@yahoo.com>
2023-08-21 21:40:13 -0500

    Resolve test connection refused issue by setting the right auth redirect path

--- tests/src/01.login.ts
+++ tests/src/01.login.ts
@@ -9,7 +9,7 @@ test.beforeAll(async ({ browser }, testInfo) => {
   const context = await browser.newContext()
   await page.goto('/')
   // await page.goto('http://localhost:3001/login?clientId=dosgato-admin-test&returnUrl=http%3A%2F%2Flocalhost%3A3000%2F.admin&requestedUrl=http%3A%2F%2Flocalhost%3A3000%2F.admin%2Fpages')
-  await page.goto('http://fakeauth-test:3001/login?clientId=dosgato-admin-test&returnUrl=http%3A%2F%2Fdosgato-admin-test%3A3000%2F.admin&requestedUrl=http%3A%2F%2Fdosgato-admin-test%3A3000%2F.admin%2F')
+  await page.goto('http://fakeauth-test/login?clientId=dosgato-admin-test&returnUrl=http%3A%2F%2Fdosgato-admin-test%3A3000%2F.admin&requestedUrl=http%3A%2F%2Fdosgato-admin-test%3A3000%2F.admin%2F')
   await page.getByPlaceholder('username').fill('system')
   await page.getByRole('button', { name: 'login' }).click()
   // await expect(page).toHaveTitle('DEV Gato Admin')
