e2e2534971c29212e96298c1fe92617eac54d375
Yuanhua Qu <yuanhuaqu@yahoo.com>
2023-09-28 15:11:13 -0500

    Adjust test for login to fit into recent change of adding applicaiton actions for screenreader

--- tests/src/01.login.ts
+++ tests/src/01.login.ts
@@ -3,8 +3,8 @@ import { test, expect } from '../fixtures'
 test('confirm logged in', async ({ adminPage, editorPage }) => {
   await adminPage.page.goto('/.admin/pages')
   await expect(adminPage.page).toHaveTitle('DEV DG Editing')
-  await expect(adminPage.greeting).toHaveText('System User')
+  await expect(adminPage.greeting).toContainText('System User')
   await editorPage.page.goto('/.admin/pages')
   await expect(editorPage.page).toHaveTitle('DEV DG Editing')
-  await expect(editorPage.greeting).toHaveText('Michael Scott')
+  await expect(editorPage.greeting).toContainText('Michael Scott')
 })
