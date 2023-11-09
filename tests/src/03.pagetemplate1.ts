93d43389f7c760926f3110c6583c55048028170c
Yuanhua Qu <yuanhuaqu@yahoo.com>
2023-11-09 14:10:46 -0600

    Test: Add more grouping for tests and pagetemplate2 test cases

--- tests/src/03.pagetemplate1.ts
+++ tests/src/03.pagetemplate1.ts
@@ -12,47 +12,51 @@ test.beforeEach(async ({ adminPage }) => {
 })
 
 test.describe('pagetemplate1 contents', () => {
-  test('should be able to add a quote', async ({ adminPage, isMobile }) => {
-    const page = adminPage.page
-    await page.getByText('Standard', { exact: true }).click();
-    await page.getByRole('button', { name: 'Quote' }).click();
-    await page.getByLabel('Author').fill('author1');
-    await page.getByLabel('Quote').fill('Get healthy life and be happy!');
-    await page.getByRole('button', { name: 'Save' }).click();
-    await expect(locateEditFrame(page).getByText('Get healthy life and be happy!- author1')).toBeVisible()
+  test.describe('standard', () => {
+    test.beforeEach(async ({ adminPage }) => {
+      await adminPage.page.getByText('Standard', { exact: true }).click();
+    })
+    test('should be able to add a quote', async ({ adminPage, isMobile }) => {
+      const page = adminPage.page
+      await page.getByRole('button', { name: 'Quote' }).click();
+      await page.getByLabel('Author').fill('author1');
+      await page.getByLabel('Quote').fill('Get healthy life and be happy!');
+      await page.getByRole('button', { name: 'Save' }).click();
+      await expect(locateEditFrame(page).getByText('Get healthy life and be happy!- author1').last()).toBeVisible()
+    })
+    test('should be able to add rich text', async ({ adminPage, isMobile }) => {
+      const page = adminPage.page
+      await page.getByRole('button', { name: 'Rich Text' }).click();
+      await page.getByLabel('Title').fill('Rich Text Title');
+      await page.getByLabel('Rich Text').fill('Where is the rich text tool bars?');
+      await page.getByRole('button', { name: 'Save' }).click();
+      await expect(locateEditFrame(page).getByRole('heading', { name: 'Rich Text Title' }).last()).toBeVisible();
+      await expect(locateEditFrame(page).getByText('Where is the rich text tool bars?').last()).toBeVisible();
+    })
+    test('should be able to add horizontal rule', async ({ adminPage, isMobile }) => {
+      const page = adminPage.page
+      await page.getByRole('button', { name: 'Horizontal Rule' }).click();
+      await expect(locateEditFrame(page).locator('hr').last()).toHaveClass('dg-horizonalrule');
+    })
+    test('should be able to add text & image', async ({ adminPage, isMobile }) => {
+      const page = adminPage.page
+      await page.getByRole('button', { name: 'Text & Image' }).click();
+      await page.getByLabel('Title').fill('Text and Image Title');
+      await page.getByLabel('Rich Text').fill('Where is rich text tool bars');
+      await page.getByRole('button', { name: 'Select Image' }).click();
+      await page.getByRole('treeitem', { name: 'site1 up down to navigate , right arrow to show children' }).locator('svg').nth(1).click();
+      await page.getByText('bobcat').click();
+      await page.getByRole('button', { name: 'Choose' }).click();
+      await page.getByRole('button', { name: 'Select Page' }).click();
+      await page.getByText('bs-site').click();
+      await page.getByRole('button', { name: 'Choose' }).click();
+      await page.getByRole('button', { name: 'Save' }).click();
+      await expect(locateEditFrame(page).getByRole('heading', { name: 'Text and Image Title' }).last()).toBeVisible();
+      await expect(locateEditFrame(page).getByText('Where is rich text tool bars').last()).toBeVisible();
+      await expect(locateEditFrame(page).getByRole('img').last()).toBeVisible();
+    })
   })
-  test('should be able to add rich text', async ({ adminPage, isMobile }) => {
-    const page = adminPage.page
-    await page.getByText('Standard', { exact: true }).click();
-    await page.getByRole('button', { name: 'Rich Text' }).click();
-    await page.getByLabel('Title').fill('Rich Text Title');
-    await page.getByLabel('Rich Text').fill('Where is the rich text tool bars?');
-    await page.getByRole('button', { name: 'Save' }).click();
-    await expect(locateEditFrame(page).getByRole('heading', { name: 'Rich Text Title' })).toBeVisible();
-    await expect(locateEditFrame(page).getByText('Where is the rich text tool bars?')).toBeVisible();
-  })
-  test('should be able to add horizontal rule', async ({ adminPage, isMobile }) => {
-    const page = adminPage.page
-    await page.getByText('Standard', { exact: true }).click();
-    await page.getByRole('button', { name: 'Horizontal Rule' }).click();
-    await expect(locateEditFrame(page).locator('hr')).toHaveClass('dg-horizonalrule');
-  })
-  test('should be able to add text & image', async ({ adminPage, isMobile }) => {
-    const page = adminPage.page
-    await page.getByText('Standard', { exact: true }).click();
-    await page.getByRole('button', { name: 'Text & Image' }).click();
-    await page.getByLabel('Title').fill('Text and Image Title');
-    await page.getByLabel('Rich Text').fill('Where is rich text tool bars');
-    await page.getByRole('button', { name: 'Select Image' }).click();
-    await page.getByRole('treeitem', { name: 'site1 up down to navigate , right arrow to show children' }).locator('svg').nth(1).click();
-    await page.getByText('bobcat').click();
-    await page.getByRole('button', { name: 'Choose' }).click();
-    await page.getByRole('button', { name: 'Select Page' }).click();
-    await page.getByText('bs-site').click();
-    await page.getByRole('button', { name: 'Choose' }).click();
-    await page.getByRole('button', { name: 'Save' }).click();
-    await expect(locateEditFrame(page).getByRole('heading', { name: 'Text and Image Title' })).toBeVisible();
-    await expect(locateEditFrame(page).getByText('Where is rich text tool bars')).toBeVisible();
-    await expect(locateEditFrame(page).getByRole('img')).toBeVisible();
+  test.describe('containers', () => {
+    
   })
 })
---
+++ tests/src/04.pagetemplate2.ts
@@ -0,0 +1,58 @@
+import { locateEditFrame } from '../common'
+import { test, expect } from '../fixtures'
+
+const TEST_SITE = {
+  name: 'site2',
+  title: 'Site 2 Home'
+}
+
+test.beforeEach(async ({ adminPage }) => {
+  await adminPage.page.goto('/.admin/pages/13')
+  await locateEditFrame(adminPage.page).getByRole('button', { name: 'Add pagetemplate2 Content' }).click();
+})
+
+test.describe('pagetemplate1 contents', () => {
+  test.describe('standard', () => {
+    test.beforeEach(async ({ adminPage }) => {
+      await adminPage.page.getByText('Standard', { exact: true }).click();
+    })
+    test('should be able to add a link', async ({ adminPage, isMobile }) => {
+      const page = adminPage.page
+      await page.getByRole('button', { name: 'Link' }).click();
+      await page.getByLabel('Text *').fill('Link Text');
+      await page.locator('form div').filter({ hasText: 'Link * Select Link Target' }).getByRole('textbox').click();
+      await page.getByRole('button', { name: 'Select Link Target' }).click();
+      await page.getByText('bs-site').click();
+      await page.getByRole('button', { name: 'Choose' }).click();
+      await page.getByRole('button', { name: 'Save' }).click();
+      await expect(locateEditFrame(page).getByRole('link', { name: 'Link Text' }).last()).toBeVisible()
+      await expect(locateEditFrame(page).getByRole('link', { name: 'Link Text' }).last()).toHaveClass('dg-link');
+      await expect(locateEditFrame(page).getByRole('link', { name: 'Link Text' }).last()).toHaveAttribute('href', '/.preview/latest/bs-site.html');
+    })
+    test('should be able to add a quote', async ({ adminPage, isMobile }) => {
+      const page = adminPage.page
+      await page.getByRole('button', { name: 'Quote' }).click();
+      await page.getByLabel('Author').fill('author2');
+      await page.getByLabel('Quote').fill('Life is beautiful!');
+      await page.getByRole('button', { name: 'Save' }).click();
+      await expect(locateEditFrame(page).getByText('Life is beautiful!- author2').last()).toBeVisible()
+    })
+    test('should be able to add rich text', async ({ adminPage, isMobile }) => {
+      const page = adminPage.page
+      await page.getByRole('button', { name: 'Rich Text' }).click();
+      await page.getByLabel('Title').fill('Rich Text Title');
+      await page.getByLabel('Rich Text').fill('Where is the rich text tool bars?');
+      await page.getByRole('button', { name: 'Save' }).click();
+      await expect(locateEditFrame(page).getByRole('heading', { name: 'Rich Text Title' }).last()).toBeVisible();
+      await expect(locateEditFrame(page).getByText('Where is the rich text tool bars?').last()).toBeVisible();
+    })
+    test('should be able to add horizontal rule', async ({ adminPage, isMobile }) => {
+      const page = adminPage.page
+      await page.getByRole('button', { name: 'Horizontal Rule' }).click();
+      await expect(locateEditFrame(page).locator('hr').last()).toHaveClass('dg-horizonalrule');
+    })
+  })
+  test.describe('containers', () => {
+
+  })
+})
