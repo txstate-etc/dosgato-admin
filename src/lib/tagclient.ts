68de3a65fac227d450e40f9ab56aa6d628a43eae
Anne Faber <anne.faber@txstate.edu>
2024-09-23 16:14:50 -0500

    update tagclient to handle an array of target links

--- src/lib/tagclient.ts
+++ src/lib/tagclient.ts
@@ -1,11 +1,20 @@
 import { api } from './api.js'
 import { type TagClient } from '@dosgato/dialog'
+import { unique } from 'txstate-utils'
 
 export class TagClientByLink implements TagClient {
   constructor (public pagetreeId?: string) {}
 
-  async availableForTarget (targetLink: string, publicTags?: boolean) {
-    return (await api.getAvailableTagsForLink(targetLink, this.pagetreeId)).filter(g => !publicTags || !g.internal)
+  async availableForTarget (targetLink: string, publicTags?: boolean)
+  async availableForTarget (targetLinks: string[], publicTags?: boolean)
+  async availableForTarget (targetLink: unknown, publicTags?: boolean) {
+    if (Array.isArray(targetLink)) {
+      const tags = await Promise.all(targetLink.map(async link => (await api.getAvailableTagsForLink(link, this.pagetreeId)).filter(g => !publicTags || !g.internal)))
+      return unique(tags.flat())
+    } else if (typeof targetLink === 'string') {
+      return (await api.getAvailableTagsForLink(targetLink, this.pagetreeId)).filter(g => !publicTags || !g.internal)
+    }
+    return []
   }
 }
 
