ee6b989f2bedd57de0ec0c03a088f0a08ab3eb31
Anne Faber <anne.faber@txstate.edu>
2025-06-30 10:25:01 -0500

    catch error during editor page loading and redirect back to /pages if it isn't a 404 issue 1121, editors should not see a 500 when trying to open a page the do not have permission to edit

--- src/routes/pages/[id]/+page.ts
+++ src/routes/pages/[id]/+page.ts
@@ -1,4 +1,4 @@
-import { error, type Load } from '@sveltejs/kit'
+import { error, type Load, redirect } from '@sveltejs/kit'
 import { base } from '$app/paths'
 import { api, pageEditorStore, subnavStore, templateRegistry } from '$lib'
 import type { PageSubNavLink } from './helpers'
@@ -13,15 +13,22 @@ function getPageIcon (type: string) {
 
 export const load: Load<{ id: string }> = async ({ params }) => {
   await templateRegistry.enhanceInfo()
-  const page = await api.getEditorPage(params.id)
-  if (!page) throw error(404)
-  const pagetemplate = templateRegistry.getTemplate(page.data.templateKey)
-  if (!pagetemplate) throw error(500, 'Unrecognized Page Template')
-  subnavStore.open('pages', { href: `${base}/pages/${page.id}`, label: page.name, icon: getPageIcon(page.pagetree.type), pageId: page.id, onClose: free })
-  toBeFreed.delete(page.id)
-  setTimeout(() => {
-    for (const pageId of toBeFreed.values()) pageEditorStore.free(pageId)
-    toBeFreed.clear()
-  }, 500)
-  return { page, pagetemplate, loaded: true }
+  try {
+    const page = await api.getEditorPage(params.id)
+    if (!page) throw error(404)
+    const pagetemplate = templateRegistry.getTemplate(page.data.templateKey)
+    if (!pagetemplate) throw error(500, 'Unrecognized Page Template')
+    subnavStore.open('pages', { href: `${base}/pages/${page.id}`, label: page.name, icon: getPageIcon(page.pagetree.type), pageId: page.id, onClose: free })
+    toBeFreed.delete(page.id)
+    setTimeout(() => {
+      for (const pageId of toBeFreed.values()) pageEditorStore.free(pageId)
+      toBeFreed.clear()
+    }, 500)
+    return { page, pagetemplate, loaded: true }
+  } catch (err) {
+    if (err.status === 404) {
+      throw error(404)
+    }
+    redirect(302, `${base}/pages`)
+  }
 }
