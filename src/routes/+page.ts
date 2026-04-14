e1b2e11e3ec9662153a8763e4a2712747053b78b
Anne Faber <anne.faber@txstate.edu>
2026-04-14 11:02:18 -0500

    users who don't have permission to see Pages should see the Dashboard when they log in

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,11 @@
 import { redirect, type Load } from '@sveltejs/kit'
 import { base } from '$app/paths'
+import { get } from 'svelte/store'
+import { globalStore } from '$lib'
 
-export const load: Load = async () => {
-  throw redirect(301, base + '/pages')
+export const load: Load = async ({ parent }) => {
+  await parent()
+  const { access } = get(globalStore)
+  if (access?.viewPageManager) throw redirect(302, base + '/pages')
+  throw redirect(302, base + '/dashboard')
 }
