033fd46a2e339aa3bfc917be88ecf6784baf8b0b
Nick Wing <nwing@txstate.edu>
2026-07-04 18:48:35 -0500

    add -l flag to test.h for running tests against local copies of api and render instead of what's committed

--- test.sh
+++ test.sh
@@ -6,4 +6,11 @@ if [ -e docker-compose.test.override.yml ]; then
   override='-f docker-compose.test.override.yml'
 fi
 
-docker-compose -p dosgato-admin-test -f docker-compose.test.yml $override up --build --abort-on-container-exit --exit-code-from testing-container
+localcompose=''
+for arg in "$@"; do
+  if [ "$arg" = "-l" ] || [ "$arg" = "--local" ]; then
+    localcompose='-f docker-compose.local.yml'
+  fi
+done
+
+docker-compose -p dosgato-admin-test -f docker-compose.test.yml $override $localcompose up --build --abort-on-container-exit --exit-code-from testing-container
